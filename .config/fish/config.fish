bb9ffa7b923cfd0275d31bf9e30e82555adfcf45
Nicolai Kaasgaard Bonde <neobonde@gmail.com>
2023-11-18 22:53:31 +0100

    add modern replacements for gnu coreutils, and clean up of bootstrap.sh

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,6 +2,28 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+starship init fish | source
+
+# Alias
+
+## config alias used to commit changes to config files
 alias config='/usr/bin/git --git-dir=/home/nkb/.cfg/ --work-tree=$HOME'
 
-starship init fish | source
+## cat -> batcat
+alias cat="batcat"
+
+## ls -> eza
+alias ls="eza --icons --hyperlink -F"
+alias tree="ls -T"
+alias lsg="ls --git-ignore"
+
+## ps -> procs
+alias ps="procs"
+
+## top -> bottom
+alias top="bottom"
+alias htop="bottom"
+alias ytop="bottom"
+
+
+
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,23 +1,51 @@
 #!/bin/bash
 
+# Installing Terminal and shell
+## Installing fish - shell
 sudo apt-add-repository -y ppa:fish-shell/release-3
-
 sudo apt update
-
-sudo apt install -y fish kitty fonts-firacode
-
-sudo update-alternatives --set x-terminal-emulator /usr/bin/kitty
-
+sudo apt install -y fish
 sudo chsh -s /usr/bin/fish
 
-mkdir ~/.fonts/
+## Installing kitty - terminal
+sudo apt install -y fish 
+sudo update-alternatives --set x-terminal-emulator /usr/bin/kitty
 
-# Install nerd font FiraCode
+## Styling packages for terminal and shell
+### Install nerd font FiraCode
+sudo apt install -y fonts-firacode 
+mkdir ~/.fonts/
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/FiraCode.zip -O temp.zip
 unzip -n temp.zip -d ~/.fonts/
 rm temp.zip
 fc-cache -fv
 
+### Installing starship cross shell prompt
 curl -sS https://starship.rs/install.sh | sh -s -- --yes
 
+# Installing neovim
+sudo apt install -y neovim 
+
+# Installing modern replacements for GNU coreutils
+## Installing bat modern replacement for cat
+sudo apt install -y bat
+
+## Installing eza modern replacement for ls
+sudo apt install -y gpg
+sudo mkdir -p /etc/apt/keyrings
+wget -qO- https://raw.githubusercontent.com/eza-community/eza/main/deb.asc | sudo gpg --dearmor -o /etc/apt/keyrings/gierens.gpg
+echo "deb [signed-by=/etc/apt/keyrings/gierens.gpg] http://deb.gierens.de stable main" | sudo tee /etc/apt/sources.list.d/gierens.list
+sudo chmod 644 /etc/apt/keyrings/gierens.gpg /etc/apt/sources.list.d/gierens.list
+sudo apt update
+sudo apt install -y eza
+
+## Installing procs modern replacement for ps
+sudo snap install procs
+
+## Installing bottom modern replacement for top
+sudo snap install bottom
+sudo snap connect bottom:mount-observe
+sudo snap connect bottom:hardware-observe
+sudo snap connect bottom:system-observe
+sudo snap connect bottom:process-control
 
